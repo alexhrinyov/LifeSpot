d138ec666bd48b4cad0b56519930afafa8e5b29f
alexhrinyov <hrinyou29@gmail.com>
2023-03-30 21:42:33 +0300

    Перенесли js в отдельный файл

---
+++ LifeSpot/Static/JS/index.js
@@ -0,0 +1,8 @@
+﻿let userAge = prompt("Внимание, контент для лиц старше 18! Сколько вам лет?");
+if (userAge < 18) {
+    alert("Извините, сходите в гугл.");
+    window.location.href = "http://www.google.com";
+}
+else {
+    alert("Приветствуем на LifeSpot! Время сейчас: " + new Date().toLocaleString());
+}
