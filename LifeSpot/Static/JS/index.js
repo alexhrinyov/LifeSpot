6fe281cc632f2f3153cbd5a3dc447544b0a2f9c8
alexhrinyov <hrinyou29@gmail.com>
2023-04-03 17:59:57 +0300

    17^59

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -6,3 +6,20 @@ if (userAge < 18) {
 else {
     alert("Приветствуем на LifeSpot! Время сейчас: " + new Date().toLocaleString());
 }
+
+function checkElement() {
+    // Получим все контейнеры с видео
+    let elements = document.getElementsByClassName('bordered');
+
+    // Пробежимся в цикле и выведем все в консоль
+    for (let i = 0; i <= elements.length; i++) {
+        // Получим всё что внутри контейнера
+        let childElements = elements[i];
+        // Получим элемент, содержащий описание видео
+        // Он у нас единственный с тегом h3, снова воспользуемся поиском по тегу,
+        let videoDescription = childElements.getElementsByTagName('h3')[0];
+        // Выведем его текст на консоль
+        console.log(videoDescription.innerText);
+    }
+    
+}
